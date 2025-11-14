cac2b78af7a4572f05e784d7fccd81cf6b6237e0
linsherpa <lincoln@lincoln.lincoln>
2025-11-14 20:31:38 +0100

    new nuxt platform

---
+++ app/components/AppFooter.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const { footer } = useAppConfig()
+</script>
+
+<template>
+  <UFooter>
+    <template #left>
+     <b> {{ footer.credits }} </b>
+    </template>
+
+    <template #right>
+      <UColorModeButton v-if="footer?.colorMode" />
+
+      <template v-if="footer?.links">
+        <UButton
+          v-for="(link, index) of footer?.links"
+          :key="index"
+          v-bind="{ color: 'neutral', variant: 'ghost', ...link }"
+        />
+      </template>
+    </template>
+  </UFooter>
+</template>
